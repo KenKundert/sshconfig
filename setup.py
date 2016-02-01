dc4b15825c09b3b1864a2b941331bcc5b7d307c1
Ken Kundert <thenurd@nurdletech.com>
2016-01-31 22:38:44 -0800

    Improve setup.py file.

--- setup.py
+++ setup.py
@@ -1,19 +1,32 @@
 from setuptools import setup
-from textwrap import dedent
 
-longDescription=dedent('''
-    Generates SSH configuration file based on what network is currently being 
-    used.
-''')
+with open('README.rst') as f:
+    readme = f.read()
 
 setup(
     name='sshconfig'
-  , version='1.0'
-  , description='generate ssh config'
-  , long_description=longDescription
+  , version='1.0.0'
+  , description='generate ssh config file'
   , author="Ken Kundert"
   , author_email='ken@designers-guide.com'
+  , url='http://nurdletech.com/linux-utilities/sshconfig'
+  , download_url='https://github.com/kenkundert/sshconfig/tarball/master'
   , scripts=['gensshconfig']
   , py_modules=['sshconfig', 'config', 'hosts', 'core']
-  , use_2to3=True
+  , license='GPLv3+'
+  , install_requires=['docopt']
+  , platforms=['linux']
+  , classifiers=[
+        'Development Status :: 3 - Alpha'
+      , 'Environment :: Console'
+      , 'Intended Audience :: End Users/Desktop'
+      , 'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
+      , 'Natural Language :: English'
+      , 'Operating System :: POSIX :: Linux'
+      , 'Programming Language :: Python :: 3.5'
+      , 'Programming Language :: Python :: 3.4'
+      , 'Programming Language :: Python :: 3.3'
+      , 'Programming Language :: Python :: 2.7'
+      , 'Topic :: Utilities'
+    ],
 )
